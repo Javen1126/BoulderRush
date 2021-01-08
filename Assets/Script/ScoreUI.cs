14a94dfe1d544b9279d5be7da8021b95e38ea3fe
Javen1126 <77139046+Javen1126@users.noreply.github.com>
2021-01-08 20:38:14 +0800

    v0.12
    -added Roboto font
    -implement Score UI based on travel distance

---
+++ Assets/Script/ScoreUI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreUI : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Transform player;
+    public Text scoreText;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.text = player.position.z.ToString("0");
+    }
+}
