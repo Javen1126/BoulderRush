a65c6680352831906d3f29ff7d9933b4a190f36c
Javen1126 <77139046+Javen1126@users.noreply.github.com>
2021-01-08 14:25:30 +0800

    v0.1
    base mechanic of the game
    -can move with accelerometer
    -collide obstacle reset position of player
    -fog lighting

---
+++ Assets/Script/PlayerCollision.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public Rigidbody rb;
+    Vector3 originalPos;
+    public PlayerMovement movement;
+    void Start()
+    {
+        originalPos = gameObject.transform.position;
+    }
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        if(collisionInfo.collider.tag == "LoseCon")
+        {
+            Debug.Log("We hit an obstacle!");
+            //movement.enabled = false;
+            rb.velocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+            transform.position = originalPos;
+        }
+    }
+}
---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float forwardForce = 200f;
+    public float sideForce = 500f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    //better to use this when calculating physics
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime); //Add forward force to z axis
+
+        transform.Translate(Input.acceleration.x * Time.smoothDeltaTime * 10, 0, 0); // Accelerometer to tilt left and right
+
+        //Add force to x axis to control movement left and right (test movement using keyboard)
+        if (Input.GetKey("right"))
+        {
+            rb.AddForce(sideForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("left"))
+        {
+            rb.AddForce(-sideForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+    }
+}
