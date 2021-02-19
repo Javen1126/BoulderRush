8e5c5156a6e06234b3fc2d00ae48b1935eb4de58
Javen1126 <77139046+Javen1126@users.noreply.github.com>
2021-02-20 01:21:37 +0800

    v0.8
    -added slider for invincible powerup charge and countdown timer

---
+++ Assets/Script/Invincible.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Invincible : MonoBehaviour
+{
+    Image invincibleBar;
+    float timeLeft;
+    float chargeAmt;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        invincibleBar = GetComponent<Image>();
+        timeLeft = FindObjectOfType<PlayerCollision>().duration;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        chargeAmt = FindObjectOfType<PlayerCollision>().addupPoint;
+        if (FindObjectOfType<PlayerCollision>().Invincible)
+        {
+            timeLeft -= Time.deltaTime;
+            invincibleBar.fillAmount = timeLeft / FindObjectOfType<PlayerCollision>().duration;
+        }
+        else
+        {
+            invincibleBar.fillAmount = chargeAmt / FindObjectOfType<PlayerCollision>().requiredPoint;
+            timeLeft = FindObjectOfType<PlayerCollision>().duration;
+        }
+    }
+}
